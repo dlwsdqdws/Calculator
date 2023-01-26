9e1a123774c6fc061703ef0546c483ac2561c534
dlwsdqdws <dlwsdqdws@gmail.com>
2023-01-26 13:14:09 +0800

    add component

---
+++ src/component/app.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import NavBar from "./NavBar";
+
+class App extends Component{
+
+    state = {}
+
+    render(){
+        return (
+            <React.Fragment>
+                <NavBar/>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from './component/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
 );
